aac4012d1fc005ca9146ea68612d21e81ed1b696
Takuma Kajikawa <kj1ktk@gmail.com>
2024-08-23 14:21:44 +0900

    chore: remove unused file

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build -t gcloud-pubsub-emulator:latest .
-docker tag gcloud-pubsub-emulator valbeat/gcloud-pubsub-emulator:latest
-docker push docker.io/valbeat/gcloud-pubsub-emulator:latest
-docker push docker.io/valbeat/gcloud-pubsub-emulator:latest
